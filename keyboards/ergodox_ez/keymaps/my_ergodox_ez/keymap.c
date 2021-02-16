3d20c5e5c89491d77610fae1ce505b15b3a3edb2
IchigoPantsu <myGithubLogin@gmail.com>
2021-02-17 02:57:59 +0900

    update:my_ergodox_ez keymap

--- keyboards/ergodox_ez/keymaps/my_ergodox_ez/keymap.c
+++ keyboards/ergodox_ez/keymaps/my_ergodox_ez/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 | Lclk | Rclk |------|       |------| Rclk | Lclk |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                                                KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
                                                         KC_TRNS,     KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
+                                      KC_BTN1, KC_BTN2, KC_TRNS,     KC_TRNS, KC_BTN2, KC_BTN1
 ),
 };
 
